bc07357c97e0b154e3a6617767b768aeb2772123
ShimaaAntar <shimaaantar901@gmail.com>
2023-01-11 00:32:36 +0200

    create Base Activity create Run Time Permission create isLocationPermissionGranted create requestLocationPermissionFromUser

--- app/src/main/java/com/example/gpstracker/MainActivity.kt
+++ app/src/main/java/com/example/gpstracker/MainActivity.kt
@@ -1,11 +1,61 @@
 package com.example.gpstracker
 
+import android.Manifest
+import android.content.DialogInterface
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.todo.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(isLocationPermissionGranted()){
+        }
+        else{
+            requestLocationPermissionFromUser()
+        }
     }
+
+    val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+
+            } else {
+
+                Toast.makeText(this,"can't find location because", Toast.LENGTH_LONG).show()
+            }
+        }
+    private fun requestLocationPermissionFromUser() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+            showDialoge(message = " app needs to access your location",
+                posActionName = "ok",
+                posAction = { dialog, which ->
+                    requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                    dialog.dismiss()
+                },
+                negActionName = "no",
+                negAction = { dialog, which ->
+                    dialog.dismiss()
+                })
+        }
+        else{
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    private fun isLocationPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(this,
+            Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED
+
+    }
+
 }
